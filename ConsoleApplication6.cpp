57c6881d7933b5badd98bb85d44710fd1f5a57fb
Stephan Michailenko <94607203+stephan-art@users.noreply.github.com>
2022-11-03 20:16:17 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+
+	int n;
+	cin >> n;
+	switch (n) {
+	case 1:
+	{
+		cout << "Russkiy";
+		break;
+	}
+	case 2:
+	{
+		cout << "Matematika";
+		break;
+	}
+	case 3:
+	{
+		cout << "Gulyat";
+		break;
+	}
+	default:
+		break;
+	}
+}
